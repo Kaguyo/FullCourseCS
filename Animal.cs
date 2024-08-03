e41b91b0e59c882fa28885add04b91f9647daa6d
Gabriel <gabrieltroop27@gmail.com>
2024-08-02 22:41:10 -0300

    Class 3 inheritance

---
+++ Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullCourseCS
+{
+    public class Animal
+    {
+        // Abstract classes = modifier that indicates missing components or incomplete implementation
+        public String id = "";
+        public double maxSpeed = 0;
+        public double speed = 0;
+        public double weight = 0;
+        public String type = "";
+        public void move(String animalId, double animalSpeed, double animalWeight, String animalType, double animalMaxSpeed)
+        {
+            if (animalType == "Predator" && animalMaxSpeed / animalSpeed <= 1.3333333333)
+            {
+                Console.WriteLine($"{animalId} is hunting! ({animalSpeed} Km/h).");
+            }
+            else if (animalType == "Prey" && animalMaxSpeed / animalSpeed <= 1.3333333333)
+            {
+                Console.WriteLine($"{animalId} is fleeing! ({animalSpeed} Km/h).");
+            }
+            else if (animalSpeed == 0) 
+            {
+                Console.WriteLine($"{animalId} is staying still.");
+            }
+            else { Console.WriteLine($"{animalId} is moving. ({animalSpeed} Km/h)."); }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FullCourseCS
     {
         static void Main(string[] args)
         {
-            bool playAula1 = true;
+            bool playAula1 = false;
             if (playAula1)
             {
                 String target = "Enemy";
@@ -51,7 +51,7 @@ namespace FullCourseCS
                                           {"Kafka", "SilverWolf", "Blade", "Tingyun", "Lynx", "Hook", "Topaz","Dr.Ratio"}
                                         };
 
-                for (int i = 0; i < personagens.GetLength(0); i++) 
+                for (int i = 0; i < personagens.GetLength(0); i++)
                 {
                     for (int j = 0; j < personagens.GetLength(1); j++)
                     {
@@ -59,19 +59,57 @@ namespace FullCourseCS
                     }
                     Console.WriteLine();
                 }
+                Console.WriteLine("\nFim da aula 1.\n");
             }// END IF
-            Console.WriteLine("\nFim da aula 1.\n");
             //  Class 1 END         ===================================================
-            Characters naruto = new Characters();
-            naruto.name = "Naruto Uzumaki";
-            naruto.age = 15;
-            naruto.Introduce();
-           
-            Car car1 = new Car("Ford", "Fusion", 2019, "Black");
-            car1.Drive();
 
-            HuntRifle huntRifle = new HuntRifle("8 Rounds Magazine", 8);
-            huntRifle.Carbine();
-        }        
+            bool playAula2 = false;
+            if (playAula2)
+            {
+                Characters naruto = new Characters();
+                naruto.name = "Naruto Uzumaki";
+                naruto.age = 15;
+                naruto.Introduce();
+
+                Car car1 = new Car("Ford", "Fusion", 2019, "Black");
+                car1.Drive();
+
+                HuntRifle huntRifle = new HuntRifle("8 Rounds Magazine", 8);
+                huntRifle.Carbine();
+
+                Console.WriteLine("\nFim da aula 2.\n");
+            }// END IF
+            //  Class 2 END         ===================================================
+            bool playAula3 = true;
+            if (playAula3)
+            {
+                // Abstract classes = modifier that indicates missing components or incomplete implementation
+                Animal deer = new Animal();
+                Animal cat = new Animal();
+                Animal dog = new Animal();
+                dog.maxSpeed = 40;
+                dog.speed = 40;
+                dog.id = "Bolt";
+                dog.weight = 40;
+                dog.type = "Predator";
+                dog.move(dog.id, dog.speed, dog.weight, dog.type, dog.maxSpeed);
+
+                cat.maxSpeed = 34;
+                cat.speed = 5;
+                cat.id = "Afonso";
+                cat.weight = 6.5f;
+                cat.type = "Predator";
+                cat.move(cat.id, cat.speed, cat.weight, cat.type, cat.maxSpeed);
+
+                deer.maxSpeed = 65;
+                deer.speed = 40;
+                deer.id = "Osvaldo";
+                deer.weight = 70.7;
+                deer.type = "Prey";
+                deer.move(deer.id, deer.speed, deer.weight, deer.type, deer.maxSpeed);
+
+                Console.WriteLine("\nFim da aula 3.\n");
+            }// END IF
+        }   //  Class 3 END         ===================================================
     }
 }
